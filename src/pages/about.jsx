a3f748f89953f30d790daf77a9f245322da406f1
Kristofer Campbell <Krisjcampb@gmail.com>
2026-04-02 22:39:13 -0700

    Changed about SmashPoint explanation

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -45,7 +45,7 @@ function About() {
                         marginTop: '0.5rem',
                         marginBottom: 0,
                     }}>
-                        A hub for the Pacific Northwest Smash community
+                        A hub for the Smash Bros. community
                     </p>
                 </div>
 
@@ -65,17 +65,16 @@ function About() {
                     }} />
 
                     <p style={{ color: '#444', lineHeight: '1.8', marginBottom: '1.5rem' }}>
-                        Welcome to the PNW Smash Hub website. Here, we aim to create a central
-                        site for people in the PNW smash scene to keep in touch with the
+                        Welcome to SmashPoint. Here, we aim to create a central
+                        site for people in the smash scene to keep in touch with the
                         latest updates on events, players, and other important developments in
                         the community. We do this as an alternative to other sites to foster
-                        a close-knit community that stretches from Oregon, Washington, British
-                        Columbia, and Idaho.
+                        a close-knit community that stretches across the world.
                     </p>
 
                     <p style={{ color: '#444', lineHeight: '1.8', marginBottom: '1.5rem' }}>
-                        The PNW scene is a special scene to many, with so much to do and see,
-                        especially with regards to smash. From Smash 64 to Ultimate and
+                        The Smash Bros. scene is a special scene to many, with so much to do and see,
+                        especially in regards to playing smash. From Smash 64 to Ultimate and
                         everything in between, there is something so special with every person
                         that chooses to compete, hang out, or even just watch online. Everyone
                         that has been a part of that has made an impact on someone somehow in
@@ -87,7 +86,7 @@ function About() {
                         As such, this website was born. We want to give everyone an even bigger
                         opportunity to grow the scene through any means they can, and what better
                         way than a streamlined place to interact with the people that play one of
-                        our favorite games in one of our favorite regions.
+                        our favorite games and push the competitive boundaries of the game we love.
                     </p>
 
                     {/* Call to action strip */}
@@ -107,7 +106,7 @@ function About() {
                                 Enjoying SmashPoint?
                             </div>
                             <div style={{ color: '#888', fontSize: '0.82rem', marginTop: '0.2rem' }}>
-                                Follow @PNWSmashHub on Twitter and share the site with your crew
+                                Follow @SmashPoint on Twitter and share the site with your crew
                             </div>
                         </div>
                         <a
