8d1de563b185016698038617699239efacc9d3cc
Kristofer Campbell <Krisjcampb@gmail.com>
2026-06-04 23:57:23 -0700

    removed duplicate line of code in import for setup-key

--- src/pages/setup-keys.jsx
+++ src/pages/setup-keys.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import React, { useState, useEffect } from 'react'
 import { Container, Alert, Button, Form, InputGroup } from 'react-bootstrap'
 import { saveAs } from 'file-saver';
 import { useNavigate } from 'react-router-dom'
