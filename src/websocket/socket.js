805a05dd111e8721ff1c761210810afe46f06df6
Kristofer Campbell <Krisjcampb@gmail.com>
2026-06-23 14:47:08 -0700

    Added authentication to websocket

--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL
-// Initialize WebSocket connection
+
 const socket = io(SOCKET_URL, {
     transports: ["websocket", "polling"],
     withCredentials: true,
+    auth: (cb) => {
+        cb({ token: localStorage.getItem('token') });
+    }
 });
 
 export default socket;
