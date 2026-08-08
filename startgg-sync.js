2a6413b6652712899508448b4f24067b909c1dc3
Kristofer Campbell <Krisjcampb@gmail.com>
2026-08-08 02:16:08 -0700

    Reduced min entrants for calendar events

--- startgg-sync.js
+++ startgg-sync.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const STARTGG_API = 'https://api.start.gg/gql/alpha';
 const SMASH_ULTIMATE_ID = 1386;
-const MIN_ENTRANTS = 128;
+const MIN_ENTRANTS = 100;
 const MONTHS_AHEAD = 2;
 
 async function fetchStartGGTournaments() {
